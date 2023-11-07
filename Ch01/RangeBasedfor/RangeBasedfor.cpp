d92085a63fe202ba25c72bae48047e01e166a7b3
HongKyoungIn <ghdruddls999@naver.com>
2023-11-07 23:21:41 +0900

    2023-11-07 Study : 참조자변수, 범위기반 for문

---
+++ Ch01/RangeBasedfor/RangeBasedfor.cpp
@@ -0,0 +1,34 @@
+﻿// RangeBasedfor.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	int aList[5] = { 10, 20, 30, 40, 50 };
+
+	// 전형적인 C 스타일 반복문
+	for(int i = 0; i < 5; ++i) {
+		cout << aList[i] << ' ';
+	}
+
+	cout << endl;
+
+	// 범위 기반 C++11 스타일 반복문
+	// 각 요소의 값을 n에 복사한다.
+
+	for(auto n : aList) { // 전체 요소에 접근, 읽기만 가능
+		cout << n << ' ';
+	}
+
+	cout << endl;
+
+	// n은 각 요소에 대한 참조다.
+	for(auto &n : aList) { // 쓰기도 가능
+		cout << n << ' ';
+	}
+
+	cout << endl;
+
+	return 0;
+}
