c07f59e7c7ce31155a5f253c0ccb9cc51d2839c1
HongKyoungIn <ghdruddls999@naver.com>
2023-11-14 18:34:49 +0900

    2023-11-14 Study : C언어의 문제점 및 클래스의 대한 개념

---
+++ Ch03/HelloOOP/HelloOOP.cpp
@@ -0,0 +1,29 @@
+﻿// 3.2 클래스 기본 문법 - 클래스를 이용해 객체지양 프로그램으로 변경
+
+#include "pch.h"
+#include <iostream>
+#include <cstdio>;
+
+// 제작자의 코드
+class USERDATA {
+public:
+    // 멤버 변수 선언
+    int nAge;
+    char szName[32];
+
+    // 멤버 함수 선언 및 정의
+    void Print(void) {
+        // nAge와 szName은 Print() 함수의 지역 변수가 아니다!
+        // 함수 내부에 선언된 지역변수가 아닌 Print() 함수가 속한 클래스의 멤버 변수
+        printf("%d, %s\n", nAge, szName);
+    }
+};
+
+// 사용자의 코드
+int main() {
+    USERDATA user = { 10, "철수" };
+
+    user.Print();
+
+    return 0;
+}
---
+++ Ch03/HelloOOP2/HelloOOP2.c
@@ -0,0 +1,22 @@
+// 제작자가 미리 구현한 코드
+
+#include <stdio.h>
+
+// 제작자의 코드
+typedef struct USERDATA {
+	int nAge;
+	char szName[32];
+} USERDATA;
+
+void PrintData(USERDATA *pUser) {
+	printf("%d, %s\n", pUser->nAge, pUser->szName);
+}
+
+// 사용자의 코드
+int main(void) {
+	USERDATA user = { 20, "철수" };
+	// printf("%d, %s\n", user.nAge, user.szName);
+	PrintData(&user);
+
+	return 0;
+}
