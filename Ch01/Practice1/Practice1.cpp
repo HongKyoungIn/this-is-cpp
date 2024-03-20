a242d7a63342d8efdf1fbba0bb2350c340cb6b8c
HongKyoungIn <ghdruddls999@naver.com>
2024-03-20 15:41:57 +0900

    Chapter1 예제

---
+++ Ch01/Practice1/Practice1.cpp
@@ -0,0 +1,17 @@
+﻿//1. 자신의 이름과 나이를 입력받고 "나의 이름은 홍길동이고 나이는 20살 입니다." 를 출력하는 프로그램을 작성하시오.
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+
+    cin >> name >> age;
+
+    cout << "나의 이름은 " << name << "이고 나이는 " << age << "살 입니다.";
+
+    return 0;
+}
---
+++ Ch01/ReferenceSwap/ReferanceSwap.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+void Swap(int &a, int &b) {
+    int nTmp = a;
+    a = b;
+    b = nTmp;
+}
+
+int main() {
+    int x = 10;
+    int y = 20;
+
+    // 참조 전달이며 두 변수의 값이 교환된다.
+    Swap(x, y);
+
+    // 교환된 결과를 출력한다.
+    cout << "x : " << x << endl;
+    cout << "y : " << y << endl;
+
+    return 0;
+}
